e38d21b28d897c4f5db244d0595e7c2237e9cb03
gummi989 <34032023+gummi989@users.noreply.github.com>
2018-04-23 11:44:32 +0000

    Add files via upload

---
+++ hrutaspil.py
@@ -0,0 +1,56 @@
+import random
+
+import math
+
+#Gudmundur Natan Hardarson
+#Dagsetning 4/23/2018
+
+class Hrutar:
+
+    def __init__(self, Þyngd, Mjólk, ull, Fjöldiafkvæma,Einkunnlæris,Frjósemi, Gerð, malir ):
+        self.Þyngd = þyngd
+        self.Mjólk = mjólk
+        self.Ull = ull
+        self.Fjöldiafkvæma = fjöædiafkvæma
+        self.Einkunnlæris = einkunnlæris
+        self.Frjósemi = frjósemi
+        self.Gerð = gerð
+        self.malir = malir
+
+    Gotti()
+
+    def __init__(self,nöfn):
+
+        self.nöfn = Gotti
+        self.nöfn = Hrói
+        self.nöfn = Dökkvi
+        self.nöfn = Bolli
+        self.nöfn = Týr
+        self.nöfn = Papi
+        self.nöfn = Tengill
+        self.nöfn = At
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
