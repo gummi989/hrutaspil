fab3344e10c96634ca510e043b7f0f1a27b0f070
gummi989 <34032023+gummi989@users.noreply.github.com>
2018-04-30 10:57:59 +0000

    Update hrutaspil.py

--- hrutaspil.py
+++ hrutaspil.py
@@ -1,34 +1,49 @@
 import random
-
 import math
-
+import time
 #Gudmundur Natan Hardarson
 #Dagsetning 4/23/2018
 
 class Hrutar:
+    def __init__(self,nafn ,thyngs, mjolk, ull, fjoldiafkvaema,einkunnlaeris,frjosemi, gerd, malir ):
+        self.nafn = nafn
+        self.thyngd
+        self.mjolk
+        self.ull
+        self.fjoldiafkvaema
+        self.einkunnlaeris
+        self.frjosemi
+        self.gerd
+        self.malir
 
-    def __init__(self, Þyngd, Mjólk, ull, Fjöldiafkvæma,Einkunnlæris,Frjósemi, Gerð, malir ):
-        self.Þyngd = þyngd
-        self.Mjólk = mjólk
-        self.Ull = ull
-        self.Fjöldiafkvæma = fjöædiafkvæma
-        self.Einkunnlæris = einkunnlæris
-        self.Frjósemi = frjósemi
-        self.Gerð = gerð
-        self.malir = malir
+    def Lysing(self):
+        print('Nafn:' ,self.nöfn)
+        print('Þyngd:' ,self.þyngd)
+        print('Mjólk:' ,self.mjólk)
+        print('Ull:' ,self.ull)
+        print('Fjöldafkvæma:' ,self.fjöldiafkvæma)
+        print('Einkunnalæris:' ,self.einkunnlæris)
+        print('Frjósemi:', self.frjósemi)
+        print('Gerð:', self.gerð)
+        print('Malir', self.malir)
 
-    Gotti()
+spilari1 = []
+spilari2 = []
+on = True
+while on:
+    ollspil = []
+    spilar1 = []
+    spilari2 = []
+    skra = open('hrutaspil.txt', 'r')
+    for lina in skra:
+        spil.append(lina)
+skra.close()
+temp = []
+spilastokkur=[]
 
-    def __init__(self,nöfn):
-
-        self.nöfn = Gotti
-        self.nöfn = Hrói
-        self.nöfn = Dökkvi
-        self.nöfn = Bolli
-        self.nöfn = Týr
-        self.nöfn = Papi
-        self.nöfn = Tengill
-        self.nöfn = At
+for stak in spil:
+    temp = stak.split(",")
+    splitstokkur.append(Hrutar(temp[0], temp[1], temp[2], temp[3], temp[4], temp[5], temp[6], temp[7], temp[8]))
 
 
 
